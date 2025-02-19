555c2d895f2d17caea41eedafdee526b16367ce9
Kotori Saki <kitartist1th@gmail.com>
2025-02-19 09:48:17 +0700

    Co-authored-by: tanatipphloihintv@gmail.com <tanatipphloihintv@gmail.com>

--- script.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.querySelector('.sidebar');
-const blurInput = document.getElementById('blurInput');
-
-blurInput.addEventListener('input', () => {
-  sidebar.style.setProperty('--blur-radius', `${blurInput.value}px`);
-});
